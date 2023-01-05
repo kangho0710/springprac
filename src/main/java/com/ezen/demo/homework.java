b2392068c97e5579adb7f749c6e0d4f47b31124f
schoolzonespeedrcer <115143610+schoolzonespeedrcer@users.noreply.github.com>
2023-01-05 19:17:17 +0900

    01/05

--- src/main/java/com/ezen/demo/homework.java
+++ src/main/java/com/ezen/demo/homework.java
@@ -1,26 +1,25 @@
 package com.ezen.demo;
 
+import java.util.Arrays;
+
 public class homework {
 	public static void main(String[] args) {
 			int[] nums = {1,10,2,7,4};
 			
+			
 			for(int i=0;i<nums.length;i++) {
 				for(int j=i+1;j<nums.length;j++) {
-					int tmp;
-					int max = nums[i];
-					int min = nums[j];
-					if(max<min){
-						tmp = max;
-						min = max;
-						max = tmp;
+					if(nums[i]>nums[j]) {
+						int tmp = nums[j];
+						nums[j] = nums[i];
+						nums[i] = tmp;
 					}
-					System.out.println(nums[i]);
 				}
 			}
 			
-//			for(int i=0;i<nums.length;i++) {
-//				System.out.println(nums[i]);
-//			}
+			for(int i=0;i<nums.length;i++) {
+				System.out.println(nums[i]);
+			}
 		}
 		
 		
