6337213ee7419b9b735705c9a24970ec6af09ebb
user <user@i7B-29>
2023-01-10 17:38:38 +0900

    01/10

---
+++ src/main/java/com/ezen/demo/api/ApiBoxOffice.java
@@ -0,0 +1,110 @@
+package com.ezen.demo.api;
+
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import com.ezen.demo.vo.PapagoParamVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ch.qos.logback.core.util.Duration;
+import lombok.extern.slf4j.Slf4j;
+	
+	// 네이버 기계번역 (Papago SMT) API 예제
+	@Component
+	@Slf4j
+	@PropertySource("classpath:env.properties")
+	public class ApiBoxOffice {
+		@Value("${movie.url}")
+		private String movieUrl = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=123814aabd95d7a78fb2ddad62733508&targetDt=";
+		@Autowired
+		ObjectMapper om;
+		private int cnt = 1;
+		
+		public static void main(String[] args) throws JsonMappingException, JsonProcessingException {
+			Instant now = Instant.now();	
+			Date date = new Date();
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+				
+				for(int i=1; i<=30; i++) {
+					Instant before = now.minus(java.time.Duration.ofDays(i));
+					String dateStr = sdf.format(date);
+					log.debug("dateStr=>{}",dateStr);
+				}
+//		        String json = get();
+//				return om.readValue(json, PapagoVO.class);
+		}
+		
+	    private String post(Map<String, String> requestHeaders, PapagoParamVO papagoParam){
+	        HttpURLConnection con = connect();
+	        String postParams = "source=" + papagoParam.getSource() + "&target=" + papagoParam.getTarget() + "&text=" + papagoParam.getText(); //원본언어: 한국어 (ko) -> 목적언어: 영어 (en)
+	        try {
+	            con.setRequestMethod("get");
+	            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+	                con.setRequestProperty(header.getKey(), header.getValue());
+	            }
+
+	            con.setDoOutput(true);
+	            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+	                wr.write(postParams.getBytes());
+	                wr.flush();
+	            }
+
+	            int responseCode = con.getResponseCode();
+	            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+	                return readBody(con.getInputStream());
+	            } else {  // 에러 응답
+	                return readBody(con.getErrorStream());
+	            }
+	        } catch (IOException e) {
+	            throw new RuntimeException("API 요청과 응답 실패", e);
+	        } finally {
+	            con.disconnect();
+	        }
+	    }
+
+	    private HttpURLConnection connect(){
+	        try {
+	            URL url = new URL(movieUrl);
+	            return (HttpURLConnection)url.openConnection();
+	        } catch (MalformedURLException e) {
+	            throw new RuntimeException("API URL이 잘못되었습니다. : " + movieUrl, e);
+	        } catch (IOException e) {
+	            throw new RuntimeException("연결이 실패했습니다. : " + movieUrl, e);
+	        }
+	    }
+
+	    private String readBody(InputStream body){
+	        InputStreamReader streamReader = new InputStreamReader(body);
+
+	        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+	            StringBuilder responseBody = new StringBuilder();
+
+	            String line;
+	            while ((line = lineReader.readLine()) != null) {
+	                responseBody.append(line);
+	            }
+
+	            return responseBody.toString();
+	        } catch (IOException e) {
+	            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+	        }
+	    }
+	}
